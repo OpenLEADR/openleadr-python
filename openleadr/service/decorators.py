c3e89d3eab1fc0e1e76f3b4e6a86a6688231ea90
Stan Janssen <stan.janssen@elaad.nl>
2020-10-22 17:06:43 +0200

    Move decorators to their own module

--- openleadr/service/decorators.py
+++ openleadr/service/decorators.py
@@ -14,10 +14,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .decorators import handler, service
-from .vtn_service import VTNService
-from .event_service import EventService
-from .poll_service import PollService
-from .registration_service import RegistrationService
-from .report_service import ReportService
-from .opt_service import OptService
+def handler(message_type):
+    """
+    Decorator to mark a method as the handler for a specific message type.
+    """
+    def _actual_decorator(decorated_function):
+        decorated_function.__message_type__ = message_type
+        return decorated_function
+    return _actual_decorator
+
+
+def service(service_name):
+    """
+    Decorator to mark a class as an OpenADR Service for a specific endpoint.
+    """
+    def _actual_decorator(decorated_function):
+        decorated_function.__service_name__ = service_name
+        return decorated_function
+    return _actual_decorator
