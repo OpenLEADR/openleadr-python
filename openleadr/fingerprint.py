fc03215f03fe1761900b7ada6d051e7a9fa26e04
Stan Janssen <stan.janssen@elaad.nl>
2020-10-22 17:10:48 +0200

    Added stand-alone fingerprint printer

---
+++ openleadr/fingerprint.py
@@ -0,0 +1,17 @@
+from argparse import ArgumentParser
+from openleadr.utils import certificate_fingerprint
+
+
+def show_fingerprint():
+    parser = ArgumentParser()
+    parser.add_argument('certificate', type=str)
+    args = parser.parse_args()
+
+    if 'certificate' in args:
+        with open(args.certificate) as file:
+            cert_str = file.read()
+            print(certificate_fingerprint(cert_str))
+
+
+if __name__ == "__main__":
+    show_fingerprint()
