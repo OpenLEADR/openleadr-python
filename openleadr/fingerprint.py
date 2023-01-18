3b35cff53ae50b1e2912eba99bbc6123b7d9c537
Stan Janssen <stan.janssen@enexis.nl>
2023-01-18 10:16:42 +0100

    Allow the fingerprint module to be executed directly

--- openleadr/fingerprint.py
+++ openleadr/fingerprint.py
@@ -11,3 +11,6 @@ def show_fingerprint():
         with open(args.certificate) as file:
             cert_str = file.read()
             print(certificate_fingerprint(cert_str))
+
+if __name__ == "__main__":
+    show_fingerprint()
