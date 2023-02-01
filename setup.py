f73ae9272f38ce14ae4a1f51375733654f35b546
Stan Janssen <stan.janssen@enexis.nl>
2023-02-01 17:33:46 +0100

    Released version 0.5.29
    This fixes various small bugs relating to stability and code examples.

--- setup.py
+++ setup.py
@@ -16,11 +16,14 @@
 
 from setuptools import setup
 
-with open('README.md', 'r', encoding='utf-8') as fh:
-    long_description = fh.read()
+with open('README.md', 'r', encoding='utf-8') as file:
+    long_description = file.read()
+
+with open('VERSION', 'r', encoding='utf-8') as file:
+    version = file.read().strip()
 
 setup(name='openleadr',
-      version='0.5.28',
+      version='0.5.29',
       description='Python3 library for building OpenADR Clients (VENs) and Servers (VTNs)',
       long_description=long_description,
       long_description_content_type='text/markdown',
