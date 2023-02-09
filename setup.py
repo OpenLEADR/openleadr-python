b5870d7fa431be94280204ff7f16daf54474a67c
Stan Janssen <stan.janssen@elaad.nl>
2023-02-09 17:36:35 +0100

    Pinned dependencies

--- setup.py
+++ setup.py
@@ -33,5 +33,5 @@ setup(name='openleadr',
       packages=['openleadr', 'openleadr.service'],
       python_requires='>=3.7.0',
       include_package_data=True,
-      install_requires=['xmltodict', 'aiohttp', 'apscheduler', 'jinja2', 'signxml==2.10.1'],
+      install_requires=['xmltodict==0.13.0', 'aiohttp>=3.8.3,<4.0.0', 'apscheduler>=3.10.0,<4.0.0', 'jinja2>=3.1.2,<4.0.0', 'signxml>=3.1.0,<3.2.0'],
       entry_points={'console_scripts': ['fingerprint = openleadr.fingerprint:show_fingerprint']})
